e24a69c992ee7b0d510dd97c93445911761e39e2
javad <javad@javads-MacBook-Air.local>
2023-06-23 12:37:44 +0330

    Some refactoring

--- service_peyk/kebernetes/commands/delete.sh
+++
@@ -1,4 +0,0 @@
-kubectl delete -f ConfigMap.yml
-kubectl delete -f Secret.yml
-kubectl delete -f Deployment.yml
-kubectl delete -f Service.yml
---
+++ service_peyk/kubernetes/commands/delete.sh
@@ -0,0 +1,4 @@
+kubectl delete -f ConfigMap.yml
+kubectl delete -f Secret.yml
+kubectl delete -f Deployment.yml
+kubectl delete -f Service.yml
