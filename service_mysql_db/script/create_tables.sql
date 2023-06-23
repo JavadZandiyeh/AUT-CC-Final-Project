d9a812816c85792c7f428da0c1c1e577a311e46c
javad <javad@javads-MacBook-Air.local>
2023-06-23 12:45:34 +0330

    Some refactor on database tables

--- service_mysql_db/script/create_tables.sql
+++ service_mysql_db/script/create_tables.sql
@@ -1,14 +1,14 @@
-CREATE TABLE Coin (
+CREATE TABLE Prices (
   coin_name VARCHAR(100),
   time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   price FLOAT,
   PRIMARY KEY (coin_name, time_stamp)
 );
 
-CREATE TABLE CoinAlertSubscription (
+CREATE TABLE AlertSubscriptions (
   email VARCHAR(100),
   coin_name VARCHAR(100),
   difference_percentage INT,
-  FOREIGN KEY (coin_name) REFERENCES Coin(coin_name),
+  FOREIGN KEY (coin_name) REFERENCES Prices(coin_name),
   PRIMARY KEY (email, coin_name)
 );
