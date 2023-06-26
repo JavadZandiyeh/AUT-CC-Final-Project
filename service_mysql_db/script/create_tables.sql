e520cf4622080d8fa86818d66e99718efc2d4be2
javad <javad@javads-MacBook-Air.local>
2023-06-26 11:08:38 +0330

    Some refactoring on tables of database: Add coin table

--- service_mysql_db/script/create_tables.sql
+++ service_mysql_db/script/create_tables.sql
@@ -1,14 +1,20 @@
-CREATE TABLE Prices (
+CREATE TABLE IF NOT EXISTS Coin (
+  coin_name VARCHAR(100),
+  PRIMARY KEY (coin_name)
+);
+
+CREATE TABLE IF NOT EXISTS Prices (
   coin_name VARCHAR(100),
   time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  price FLOAT,
+  price FLOAT NOT NULL,
+  FOREIGN KEY (coin_name) REFERENCES Coin(coin_name),
   PRIMARY KEY (coin_name, time_stamp)
 );
 
-CREATE TABLE AlertSubscriptions (
+CREATE TABLE IF NOT EXISTS AlertSubscriptions (
   email VARCHAR(100),
   coin_name VARCHAR(100),
-  difference_percentage INT,
-  FOREIGN KEY (coin_name) REFERENCES Prices(coin_name),
+  difference_percentage INT NOT NULL,
+  FOREIGN KEY (coin_name) REFERENCES Coin(coin_name),
   PRIMARY KEY (email, coin_name)
 );
