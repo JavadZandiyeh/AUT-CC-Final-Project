fe90ba0c1b9c7f51d3a32f90a8e87de96aab8c3f
javad <javad@javads-MacBook-Air.local>
2023-06-29 17:53:38 +0330

    Beautify front-end

--- service_react/app_react/src/App.jsx
+++ service_react/app_react/src/App.jsx
@@ -10,61 +10,106 @@ function App() {
     borderRadius: "12px",
     border: "none",
   };
+
+  // Price History Form
+  const [coin_name_price, setCoinNamePrice] = useState("");
+  const [price_data, setPriceData] = useState(null);
+
+  // Subscription Form
   const [email, setEmail] = useState("");
-  const [coin, setCoin] = useState("");
-  const [percent, setPercent] = useState(0);
+  const [coin_name_subscription, setCoinName] = useState("");
+  const [difference_percentage, setDifferencePercentage] = useState(0);
 
-  const [coinToGetData, setCoinToGetData] = useState("");
-  const [coinData, setCoinData] = useState(null);
-
-  function handleSubscribe(e) {
+  function handlePriceForm(e) {
     e.preventDefault();
-    axios
-      .post(apiUrl + "subscribe/", {
-        email: email,
-        coin_name: coin,
-        difference_percentage: percent,
-      })
-      .then((res) => {
+    axios.post(apiUrl + "price/", {
+        coin_name: coin_name_price
+      }
+    ).then((res) => {
         if (res.status) {
-          alert("Subscription Succeed");
+          setPriceData(res.data.message);
         }
-      })
-      .catch((err) => {
+      }
+    ).catch((err) => {
         alert(err.response.data.message);
-      });
+      }
+    );
   }
 
-  function handleGetCoin(e) {
+  function handleSubscriptionForm(e) {
     e.preventDefault();
-    axios
-      .post(apiUrl + "price/", {
-        coin_name: coinToGetData
-      })
-      .then((res) => {
+    axios.post(
+      apiUrl + "subscribe/", 
+      {
+        email: email,
+        coin_name: coin_name_subscription,
+        difference_percentage: difference_percentage,
+      }
+    ).then((res) => {
         if (res.status) {
-          setCoinData(res.data.message);
+          alert(res.request.response);
         }
-      })
-      .catch((err) => {
-        alert(err);
-      });
+      }
+    ).catch((err) => {
+        alert(err.response.data.message);
+      }
+    );
   }
   
   return (
     <div>
       <div
         style={{
-          width: "100vw",
-          height: "100vh",
+          width: "80vw",
+          height: "80vh",
           display: "flex",
           alignItems: "start",
         }}
       >
         <div
           style={{
+            width: "70%",
+            padding: "0 10%",
+          }}
+        >
+          <form
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "2rem",
+              textAlign: "center",
+            }}
+            onSubmit={handlePriceForm}
+          >
+            <h2>Price History Form</h2>
+            <input
+              style={inputStyles}
+              type="text"
+              placeholder="Coin Name"
+              value={coin_name_price}
+              onChange={(e) => setCoinNamePrice(e.target.value)}
+              required
+            />
+            <input
+              style={{
+                ...inputStyles,
+                boxSizing: "content-box",
+                cursor: "pointer",
+                backgroundColor: "#50b",
+              }}
+              type="submit"
+              value="Submit"
+            />
+          </form>
+          {price_data?.map((item, index) => (
+            <div key={index}>
+              <span>{item.coin_name} / {item.price} / {item.time_stamp}</span>
+            </div>
+          ))}
+        </div>
+        <div
+          style={{
             width: "30%",
-            padding: "0 5%",
           }}
         >
           <form
@@ -74,7 +119,7 @@ function App() {
               textAlign: "center",
               gap: "1rem",
             }}
-            onSubmit={handleSubscribe}
+            onSubmit={handleSubscriptionForm}
           >
             <h2>Subscription Form</h2>
             <input
@@ -89,18 +134,18 @@ function App() {
               style={inputStyles}
               type="text"
               placeholder="Coin Name"
-              value={coin}
-              onChange={(e) => setCoin(e.target.value)}
+              value={coin_name_subscription}
+              onChange={(e) => setCoinName(e.target.value)}
               required
             />
             <input
               style={inputStyles}
               placeholder="Percentage"
               type="number"
-              value={percent}
+              value={difference_percentage}
               min={0}
               max={100}
-              onChange={(e) => setPercent(e.target.value)}
+              onChange={(e) => setDifferencePercentage(e.target.value)}
               required
             />
             <input
@@ -108,52 +153,12 @@ function App() {
                 ...inputStyles,
                 boxSizing: "content-box",
                 cursor: "pointer",
-                backgroundColor: "#22f",
+                backgroundColor: "#50b",
               }}
               type="submit"
               value="Submit"
             />
           </form>
-        </div>
-        <div
-          style={{
-            width: "50%",
-          }}
-        >
-          <form
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "1rem",
-              textAlign: "center",
-            }}
-            onSubmit={handleGetCoin}
-          >
-            <h2>Coin History</h2>
-            <input
-              style={inputStyles}
-              type="text"
-              placeholder="Coin Name"
-              value={coinToGetData}
-              onChange={(e) => setCoinToGetData(e.target.value)}
-              required
-            />
-            <input
-              style={{
-                ...inputStyles,
-                boxSizing: "content-box",
-                cursor: "pointer",
-                backgroundColor: "#22f",
-              }}
-              type="submit"
-              value="Submit"
-            />
-          </form>
-          {coinData?.map((item, index) => (
-            <div key={index}>
-              <span>time_stamp: {item.time_stamp}</span>&nbsp;<span>| price: {item.price} | coin_name: {item.coin_name}</span>
-            </div>
-          ))}
         </div>
       </div>
     </div>
